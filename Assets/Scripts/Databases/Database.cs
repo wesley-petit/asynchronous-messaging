b2e6d0a728398ff2d66883f53226de79dba88a09
Wesley Petit <ptitwes.45@gmail.com>
2020-11-08 17:17:48 +0100

    Transfer Relational database to a basic JSON content
    GetMessage and Insert are more easier to use and produce less garbage collector

--- Assets/Scripts/Databases/Database.cs
+++ Assets/Scripts/Databases/Database.cs
@@ -2,34 +2,27 @@
 using System.Collections.Generic;
 
 // Each name (attribute and type) must be the same as the JSON
+// Each Save and Load value must be marked as a SerializeField
 [System.Serializable]
 public class Database
 {
-	[SerializeField] private List<Message> messages = new List<Message>();
-	[SerializeField] private List<Position_Messages> positions_messages = new List<Position_Messages>();
-	[SerializeField] private List<User> users = new List<User>();
+	[SerializeField] private List<Message> messages;
 	[SerializeField] private string[] messages_premades = new string[0];
 
-	#region Getter
 	public List<Message> Messages => messages;
-	public List<Position_Messages> Positions_Messages => positions_messages;
-	public List<User> Users => users;
-	public string[] Messages_Premades => messages_premades;
-	#endregion
+	public string[] MessagesPremades => messages_premades;
 
+	#region Constructor
 	public Database()
 	{
 		messages = new List<Message>();
-		positions_messages = new List<Position_Messages>();
-		users = new List<User>();
 		messages_premades = new string[0];
 	}
 
-	public Database(List<Message> messages, List<Position_Messages> positions_messages, List<User> users, string[] messages_premades)
+	public Database(List<Message> messages, string[] messages_premades)
 	{
 		this.messages = messages;
-		this.positions_messages = positions_messages;
-		this.users = users;
 		this.messages_premades = messages_premades;
 	}
+	#endregion
 }
--- Assets/Scripts/Databases/DatabaseController.cs
+++ Assets/Scripts/Databases/DatabaseController.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 // Give Access in data
 public class DatabaseController : MonoBehaviour
 {
-	[SerializeField] private Database _mainDatabase = new Database();
+	[SerializeField] private Database _database = new Database();
+	[SerializeField] private float _aroundThreshold = 5f;			// Define when a message is near a player
 
 	// Wraper
-	public string[] Messages_Premades => _mainDatabase.Messages_Premades;
-
-	// To generate a new Id with the max id value
-	private int _maxMessageId = 0;
-	private int _maxPositionMessageId = 0;
-	private int _maxUserId = 0;
-	private float _aroundThreshold = 5f;							// Define when a message is near a player
+	public string[] GetMessagesPremades => _database.MessagesPremades;
+	
+	private MessageComparer _comparer = new MessageComparer();      // To verify doublons
 
 	private void Awake()
 	{
@@ -23,88 +19,52 @@ public class DatabaseController : MonoBehaviour
 		ClearDoublons();
 	}
 
-	#region Get Element By Predicate
-	public HashSet<Message> GetMessagesByPlayerPosition(Vector3 playerPosition)
+	#region ServerInteract
+	public Message[] GetMessagesByPlayerPosition(Vector3 playerPosition)
 	{
-		// Doesn't allow doublons and it's more efficient to add an item (like a List)
-		HashSet<Message> messages = new HashSet<Message>();
-		Message currentMessage = new Message();
-		
-		// Take all position in a certain area around the player
-		Position_Messages[] positions = _mainDatabase.Positions_Messages.Where(
-												k => Vector3.Distance(k.PositionMessage, playerPosition) < _aroundThreshold)
-												.ToArray();
+		/// HashSet - Doesn't allow doublons and it's more efficient to add an item (like a List)
+		/// Linq - Take all position in a certain area around the player
+		/// ToArray - HashSet can't be serialize in editor or in a file
+		return new HashSet<Message>(_database.Messages
+								.Where(k => Vector3.Distance(k.GetPositionMessage, playerPosition) < _aroundThreshold),
+								_comparer)
+								.ToArray();
+	}
 
-		// Take all messages in this positions
-		foreach (var position in positions)
+	public void Insert(Message message)
+	{
+		Logger.Write($"Add a new Message...");
+
+		// We verify his value
+		if (message == null)
 		{
-			currentMessage = GetMessageByPredicate(k => k.Id() == position.MessageId);
-			
-			if (!currentMessage.IsNull())
-			{
-				messages.Add(currentMessage);
-			}
+			Logger.Write($"Can't add a null Message", LogType.ERROR);
+			return;
 		}
 
-		return messages;
-	}
-
-	// Easier to use
-	public Message GetMessageByPredicate(Func<Message, bool> predicate) => GetElementByPredicate(_mainDatabase.Messages, predicate);
-	public Position_Messages GetPositionByPredicate(Func<Position_Messages, bool> predicate) => GetElementByPredicate(_mainDatabase.Positions_Messages, predicate);
-	public User GetUserByPredicate(Func<User, bool> predicate) => GetElementByPredicate(_mainDatabase.Users, predicate);
-	private T GetElementByPredicate<T>(List<T> searchList, Func<T, bool> predicate) => searchList.Where(predicate).FirstOrDefault();
-	#endregion
-
-	#region Insert Element
-	public void InsertMessage(string messageContent, string messageTime, int userId)
-	{
-		int messageId = GenerateId(++_maxMessageId);
-
-		_mainDatabase.Messages.Add(new Message(messageId, messageContent, messageTime, userId));
-		Logger.Write($"Add a new Message with id {messageId}");
-	}
-
-	public void InsertPositionMessage(Vector3 positionMessage, Vector3 rotationMessage, int messageId)
-	{
-		int positionId = GenerateId(++_maxPositionMessageId);
-
-		_mainDatabase.Positions_Messages.Add(new Position_Messages(positionId, positionMessage, rotationMessage, messageId));
-		Logger.Write($"Add a new Position Message with id {positionId}");
-	}
-
-	public void InsertUser(string userName)
-	{
-		int userId = GenerateId(++_maxUserId);
-
-		_mainDatabase.Users.Add(new User(userId, userName));
-		Logger.Write($"Add a new User with id {userId}");
-	}
-	#endregion
-
-	// If a foreign key exist or not
-	#region Is This Element Exist
-	// Easier to use
-	public bool IsThisMessageInDatabase(int searchId) => IsThisInDatabase(_mainDatabase.Messages, k => k.Id() == searchId, searchId);
-	public bool IsThisPositionMessageInDatabase(int searchId) => IsThisInDatabase(_mainDatabase.Positions_Messages, k => k.Id() == searchId, searchId);
-	public bool IsThisUserInDatabase(int searchId) => IsThisInDatabase(_mainDatabase.Users, k => k.Id() == searchId, searchId);
-	private bool IsThisInDatabase<T>(List<T> searchList, Func<T, bool> predicate, int searchId)
-	{
-		bool res = searchList.Any(predicate);
-		if (!res)
+		if (message.ContainsNullValues)
 		{
-			Logger.Write($"Unknown {typeof(T)} - Id {searchId} doesn't exist", LogType.ERROR);
+			Logger.Write($"Can't add a Message which contains null values", LogType.ERROR);
+			return;
 		}
 
-		return res;
+		if (IsItInDatabase(message))
+		{
+			Logger.Write($"Can't add a Message as a doublon", LogType.ERROR);
+			return;
+		}
+
+		_database.Messages.Add(message);
+		Logger.Write($"Add a new Message created by {message.GetUserName} in {message.GetPositionMessage}");
 	}
 	#endregion
 
+	#region SaveAndLoad
 	[ContextMenu("Save Database")]
 	public void SaveDatabase()
 	{
 		Logger.Write("Save Database...");
-		string jsonData = JsonUtility.ToJson(_mainDatabase, true);
+		string jsonData = JsonUtility.ToJson(_database, true);
 		FileManagement.Write(FileNameConst.DATABASE, jsonData);
 	}
 
@@ -113,51 +73,29 @@ public class DatabaseController : MonoBehaviour
 	public void LoadDatabase()
 	{
 		Logger.Write("Load Database...");
+
 		string jsonData = FileManagement.Read(FileNameConst.DATABASE);
 		if (jsonData == "")
 		{
 			Logger.Write("Failed to Load Database...", LogType.ERROR);
 			return;
 		}
-
-		_mainDatabase = JsonUtility.FromJson<Database>(jsonData);
-		InitializeMaxId();
+		_database = JsonUtility.FromJson<Database>(jsonData);
 	}
 
 	[ContextMenu("Clear Doublons")]
 	public void ClearDoublons()
 	{
 		Logger.Write("Clear Doublons");
+
 		//HashSet delete doublons, produce less garbage collector than Linq
-		List<Message> messages = new HashSet<Message>(_mainDatabase.Messages).ToList();
-		List<Position_Messages> positions_messages = new HashSet<Position_Messages>(_mainDatabase.Positions_Messages, new PositionMessageComparer()).ToList();
-		List<User> users = new HashSet<User>(_mainDatabase.Users).ToList();
-		string[] messages_premades = new HashSet<string>(_mainDatabase.Messages_Premades).ToArray();
+		List<Message> messages = new HashSet<Message>(_database.Messages, _comparer).ToList();
+		string[] messagesPremades = new HashSet<string>(_database.MessagesPremades).ToArray();
 
-		_mainDatabase = new Database(messages, positions_messages, users, messages_premades);
+		_database = new Database(messages, messagesPremades);
 	}
+	#endregion
 
-	private void InitializeMaxId()
-	{
-		_maxMessageId = GetMaxValue(_mainDatabase.Messages, k => k.Id());
-		_maxPositionMessageId = GetMaxValue(_mainDatabase.Positions_Messages, k => k.Id());
-		_maxUserId = GetMaxValue(_mainDatabase.Users, k => k.Id());
-
-		Logger.Write("Initializing Max Id");
-	}
-
-	// Take the max value
-	private int GetMaxValue<T>(List<T> list, Func<T, int> predicate) => list.Max(predicate);
-
-	private int GenerateId(int maxId)
-	{
-		// Initialize Max Id
-		if (maxId <= 0)
-		{
-			InitializeMaxId();
-		}
-
-		// Increment only on local
-		return maxId;
-	}
+	// To verify if a Message will be add as a doublon
+	private bool IsItInDatabase(Message message) => _database.Messages.Contains(message, _comparer);
 }
