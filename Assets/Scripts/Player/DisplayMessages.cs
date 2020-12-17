250272f73cf216ee73974fb3b421a6204aa35a2c
Farewell_ <norththelittlefox@gmail.com>
2020-12-17 21:35:34 +0100

    Messages

--- Assets/Scripts/Player/DisplayMessages.cs
+++ Assets/Scripts/Player/DisplayMessages.cs
@@ -1,23 +1,25 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class DisplayMessages : MonoBehaviour
 {
 	[SerializeField] private Dialog _messagePrefab = null;
 	[SerializeField] private GameObject _parent = null;
+	private List<string> _list = new List<string>();
 
 	public void DisplayMessagesFromList(Message[] messageList)
 	{
-		foreach (Transform child in _parent.transform)
-		{
-			Destroy(child.gameObject);
-		}
-
 		foreach (Message message in messageList)
 		{
-			Dialog messageObject = Instantiate(_messagePrefab, message.GetPositionMessage, Quaternion.identity);
-			messageObject.transform.SetParent(_parent.transform);
-			messageObject.name = message.GetUserName + " | " + message.GetMessageTime + " | " + message.GetMessageContent;
-			messageObject.SetDialog(message);
+			if (!_list.Contains(message.GetUserName + " | " + message.GetMessageTime + " | " + message.GetMessageContent))
+			{
+				_list.Add(message.GetUserName + " | " + message.GetMessageTime + " | " + message.GetMessageContent);
+
+				Dialog messageObject = Instantiate(_messagePrefab, message.GetPositionMessage, Quaternion.identity);
+				messageObject.transform.SetParent(_parent.transform);
+				messageObject.name = message.GetUserName + " | " + message.GetMessageTime + " | " + message.GetMessageContent;
+				messageObject.SetDialog(message);
+			}
 		}
 	}
 }
